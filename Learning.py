73e511d36dd3b986c85cd857762ad2f18d531255
100103067 <100103067@lubbockisd.net>
2019-09-30 15:50:33 -0500

    playing with friends

--- Learning.py
+++ Learning.py
@@ -1,3 +1,12 @@
+# What is an octothorpe?
+
+
 print("hello world,")
 print("Howdy, y'all!")
 print("I like typing this")
+# Practice with Maths and things
+print("I like chickens")
+print("Hens", 25 + 30/6)
+print ("Roosters:", 100 - 25 * 3 % 4)
+print("Now, I will count the eggs:")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
