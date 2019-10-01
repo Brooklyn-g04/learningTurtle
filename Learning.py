2fc281a213796749a06315d5d92eea33c0e60236
100103067 <100103067@lubbockisd.net>
2019-10-01 15:39:05 -0500

    playing with friends

--- Learning.py
+++ Learning.py
@@ -9,6 +9,7 @@ print("I like chickens")
 # Down below is an equation that the program calculates using print without the " "
 print ("Roosters:", 100 - 25 * 3 % 4)
 # Down below is a floating point which is a real number with a decimal point dividing the integer and fractional parts.
+# To make a floating point you need this / symbol
 print("Hens", 25 + 30/6)
 # Down below is another equation that the program will calculate after running it
 print("Now, I will count the eggs:")
