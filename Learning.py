8d71c9b9641cba9ffb5fa4336cd1cf77e8427e2c
100103067 <100103067@lubbockisd.net>
2019-10-01 15:37:16 -0500

    playing with friends

--- Learning.py
+++ Learning.py
@@ -1,13 +1,16 @@
 # What is an octothorpe?
 # Notes, also known in the programming community as comments
-# Down below are comments
+# Down below is what happens when you use print
 print("hello world,")
 print("Howdy, y'all!")
 print("I like typing this")
 # Practice with Maths and things
 print("I like chickens")
-print("Hens", 25 + 30/6)
+# Down below is an equation that the program calculates using print without the " "
 print ("Roosters:", 100 - 25 * 3 % 4)
+# Down below is a floating point which is a real number with a decimal point dividing the integer and fractional parts.
+print("Hens", 25 + 30/6)
+# Down below is another equation that the program will calculate after running it
 print("Now, I will count the eggs:")
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
