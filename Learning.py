a2da7bf6f62ca126c25f281b376627a3277bb8d5
100103067 <100103067@lubbockisd.net>
2019-10-01 15:24:46 -0500

    playing with friends

--- Learning.py
+++ Learning.py
@@ -1,6 +1,6 @@
 # What is an octothorpe?
-
-
+# Notes, also known in the programming community as comments
+# Down below are comments
 print("hello world,")
 print("Howdy, y'all!")
 print("I like typing this")
@@ -10,3 +10,4 @@ print("Hens", 25 + 30/6)
 print ("Roosters:", 100 - 25 * 3 % 4)
 print("Now, I will count the eggs:")
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
