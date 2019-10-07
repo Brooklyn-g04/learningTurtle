f8c30a88447641334e651dea6f49dc772b0645d0
100103067 <100103067@lubbockisd.net>
2019-10-07 15:09:54 -0500

    making our own story

--- variables.py
+++ variables.py
@@ -13,7 +13,11 @@ myFriends = 2
 # d is for numbers
 # f is a floating point
 
-
+print("let's talk about %s" % myName)
+print("Although %s has %s skills she still has %f friends." % (myName, mySkills, myFriends))
+print("%s currently is a %s and her parents hate her for it." % (myName, myRelationship))
+print("She has a %d in each of her classes because that is how low her standards are." % myGrades)
+print(" She loves her %s and rolls around in it and now she will go to bed in her %s goodnight." %(myHouse,myHouse))
 
 
 
